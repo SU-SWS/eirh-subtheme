2bafeed2fee1a3962d721af8fab065a77f4acd58
rebeccahongsf <rebeccahongsf@gmail.com>
2024-03-18 12:25:06 -0700

    quick styles for filter chips

--- src/eventshub/src/component/FilterChips/FilterChips.tsx
+++ src/eventshub/src/component/FilterChips/FilterChips.tsx
@@ -7,6 +7,7 @@ import {
 } from '../../redux/slices/filtersSlice';
 import { AppDispatch, RootState } from '../../redux/store';
 import { EventFeatureGroupItem } from '../../utilities/algoliaFiltersData';
+import { XMarkIcon} from '@heroicons/react/16/solid'
 
 export const FilterChips = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -27,8 +28,11 @@ export const FilterChips = () => {
       <Heading as='h4' size='base'>Filter By</Heading>
       <div>
         {selectedFilters.map((filter, index) => (
-          <button key={index} onClick={() => removeEventFilter(filter)}>
+          <button key={index} onClick={() => removeEventFilter(filter)}
+          className="inline-block group hover:underline focus-visible:underline transition text-16 pt-7 pb-8 px-18 font-regular rounded-full shadow hover:shadow-md focus:shadow active:shadow no-underline underline-offset-2 leading-display bg-white hocus:bg-digital-blue-20 text-digital-blue hocus:text-digital-blue-dark border-2 border-digital-blue focus:ring-2 active:ring-2 focus:ring-digital-blue-40 active:ring-digital-blue-40 outline-none hocus:decoration-digital-blue hocus:decoration-1"
+          >
             {filter.event_feature}
+            <XMarkIcon className='w-1em inline-block ml-3' />
           </button>
         ))}
       </div>
