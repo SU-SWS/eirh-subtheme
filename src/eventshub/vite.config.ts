86a27cde78a0bb4b6d3a686dfd92efc1795c0ab8
Shea McKinney <sheamck@stanford.edu>
2024-03-22 16:56:41 -0700

    Export options.

--- src/eventshub/vite.config.ts
+++ src/eventshub/vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: ['es2020'],
+    emptyOutDir: true,
     rollupOptions: {
       output: {
         entryFileNames: 'eventshub.js',
