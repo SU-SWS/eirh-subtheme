2dbee7ac38e0b2779f3a13d47a710fe861cb8591
rebeccahongsf <rebeccahongsf@gmail.com>
2024-03-27 10:03:08 -0700

    wip

--- src/eventshub/src/component/SearchFilter/SearchFilter.tsx
+++ src/eventshub/src/component/SearchFilter/SearchFilter.tsx
@@ -49,7 +49,7 @@ const SearchFilter: React.FC<SearchFilterProps> = ({
           aria-controls='accordion-content'
           className='er-flex er-direction-row er-justify-between er-w-full'
         >
-          <Heading as='h4' size='base'>
+          <Heading as='h4' size='base' className="er-m-0">
             {title}
           </Heading>
           <ChevronDownIcon
@@ -63,12 +63,12 @@ const SearchFilter: React.FC<SearchFilterProps> = ({
         aria-labelledby='accordion-button'
         hidden={!isOpen}
         role='region'
-        className={cnb(isOpen ? 'er-block' : 'er-hidden', 'er-px-26')}
+        className={cnb(isOpen ? 'er-block' : 'er-hidden', 'er-px-26 er-pt-36')}
       >
         {filterOptions.map((filter) => (
           <label
             key={filter.event_feature}
-            className='er-flex er-items-center er-cursor-pointer er-text-19 hocus:er-underline'
+            className='er-mt-0 er-mb-26 last:er-mb-0 er-flex er-items-center er-cursor-pointer er-text-19 hocus:er-underline'
           >
             <input
               type='checkbox'
@@ -76,7 +76,7 @@ const SearchFilter: React.FC<SearchFilterProps> = ({
                 (f) => f.event_feature === filter.event_feature
               )}
               onChange={() => handleFilterToggle(filter)}
-              className='er-block er-rounded er-border-2 er-border-black-50 focus:er-border-lagunita checked:er-text-lagunita checked:er-border-lagunita-light checked:hover:er-text-lagunita-dark checked:focus:er-text-lagunita-dark checked:er-hover:er-border-lagunita checked:focus:er-border-lagunita group-hover:er-bg-transparent focus:er-bg-transparent outline-none focus-visible:er-outline-none outline-none focus:er-ring-0 focus:er-ring-offset-0 checked:er-group-hover:er-text-lagunita-dark checked:focus:er-text-lagunita-dark checked:er-group-hover:er-bg-lagunita-dark checked:focus:er-bg-lagunita-dark disabled:er-border-black-40 disabled:er-pointer-events-none disabled:er-checked:bg-black-40 transition-colors w-20 h-20 indeterminate:!er-bg-lagunita indeterminate:!er-border-lagunita-light indeterminate:group-hover:!er-bg-lagunita-dark indeterminate:group-hover:!er-border-lagunita indeterminate:focus:!bg-lagunita-dark indeterminate:focus:!er-border-lagunita'
+              className='er-mr-10 er-block er-rounded er-border-2 er-border-black-50 focus:er-border-digital-blue checked:er-text-digital-blue checked:er-border-digital-blue-light checked:hover:er-text-digital-blue-dark checked:focus:er-text-digital-blue-dark checked:er-hover:er-border-digital-blue checked:focus:er-border-digital-blue group-hover:er-bg-transparent focus:er-bg-transparent outline-none focus-visible:er-outline-none outline-none focus:er-ring-0 focus:er-ring-offset-0 checked:er-group-hover:er-text-digital-blue-dark checked:focus:er-text-digital-blue-dark checked:er-group-hover:er-bg-digital-blue-dark checked:focus:er-bg-digital-blue-dark disabled:er-border-black-40 disabled:er-pointer-events-none disabled:checked:er-bg-black-40 er-transition-colors er-w-20 er-h-20'
             />
             <span>{filter.event_feature}</span>
           </label>
